7e7a13725a52db5723a1980a8390de3fbd03199f
PRABHASH RANJAN <prabhashmickey@gmail.com>
2022-08-08 00:07:25 +0530

    initial commit

---
+++ lib/sign.dart
@@ -0,0 +1,190 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:login/login.dart';
+import 'package:login/main.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+
+class sign extends StatefulWidget {
+  const sign({Key? key}) : super(key: key);
+
+  @override
+  State<sign> createState() => _signState();
+}
+
+class _signState extends State<sign> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  late bool _sucess;
+  late String _userEmail;
+
+  get user => null;
+  void _register() async {
+    final User? user = (await _auth.createUserWithEmailAndPassword(
+            email: _emailController.text, password: _passwordController.text))
+        .user;
+
+    if (user != null) {
+      setState(() {
+        _sucess = true;
+        _userEmail = user.email!;
+      });
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    } else {
+      setState(() {
+        _sucess = false;
+        Fluttertoast.showToast(msg: "USER ALREADY EXIST");
+      });
+    }
+  }
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  checkvalidation(GlobalKey<FormState> _formkey) {
+    if (_formkey.currentState!.validate()) {
+      print('VALIDATED');
+    } else {
+      print('NOT VALIDATED');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('assets/l.png'),
+        fit: BoxFit.cover,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              child: Text(
+                "SIGN UP",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              padding: EdgeInsets.only(left: 30, top: 100),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.36,
+                    right: 35,
+                    left: 35),
+                child: Form(
+                  key: _formkey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator:
+                              RequiredValidator(errorText: "ENTER ADMIN NAME"),
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'ADMIN NAME',
+                            prefixIcon: Icon(Icons.person),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: "Required"),
+                            EmailValidator(errorText: "Not a valid email"),
+                          ]),
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'EMAIL',
+                            prefixIcon: Icon(Icons.mail),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: MinLengthValidator(6,
+                              errorText: "Should be at least 6 digit"),
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            fillColor: Colors.grey.shade100,
+                            filled: true,
+                            hintText: 'PASSWORD',
+                            prefixIcon: Icon(Icons.lock),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [],
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            primary: Colors.white,
+                            backgroundColor: Color.fromARGB(255, 33, 243, 58),
+                            textStyle: TextStyle(fontSize: 30),
+                            elevation: 20,
+                            padding: EdgeInsets.all(20),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0))),
+                        onPressed: () async {
+                          checkvalidation(_formkey);
+                          _register();
+                          if (user != null) {
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                          } else {
+                            Fluttertoast.showToast(msg: "MISMATCH FIELDS");
+                          }
+                        },
+                        child: Text("Sign In : ) "),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
